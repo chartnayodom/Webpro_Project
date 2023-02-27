d1709502039264e329b0911ba983577835118284
Baipor <baipor2545@hotmail.com>
2023-02-27 22:34:30 +0700

    Let's go

---
+++ Project_R/recommented_shop.js
@@ -0,0 +1,31 @@
+const r_shop = JSON.parse(`[
+    {
+        "r_shop_id" : 00001,
+        "r_shop_name" : "John Rider",
+        "r_shop_address" : "99/99 ถนนเลียบคลองสอง",
+        "r_shop_brand" : "all",
+        "r_shop_score" : 5
+    },
+    {
+        "r_shop_id" : 00001,
+        "r_shop_name" : "John Rider",
+        "r_shop_address" : "99/99 ถนนเลียบคลองสอง",
+        "r_shop_brand" : "all",
+        "r_shop_score" : 5
+    },
+    {
+        "r_shop_id" : 00001,
+        "r_shop_name" : "John Rider",
+        "r_shop_address" : "99/99 ถนนเลียบคลองสอง",
+        "r_shop_brand" : "all",
+        "r_shop_score" : 5
+    },
+    {
+        "r_shop_id" : 00001,
+        "r_shop_name" : "John Rider",
+        "r_shop_address" : "99/99 ถนนเลียบคลองสอง",
+        "r_shop_brand" : "all",
+        "r_shop_score" : 5
+    },
+    
+]`)
