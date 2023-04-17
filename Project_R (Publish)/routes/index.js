20b8e9664085bc193b235457234f77307789eb35
Baipor <baipor2545@hotmail.com>
2023-04-18 01:16:29 +0700

    add backend router
    เพิ่มrouter สำหรับbackend แต่ยังไม่ได้เชื่อมDatabase ไฟล์SQL จะอัพภายหลัง

--- Project_R (Publish)/routes/index.js
+++ Project_R (Publish)/routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const pool = require("../config");
+const pool = require("../config");
 
 router = express.Router();
 
@@ -7,6 +7,35 @@ router.get("/", async function(req,res,next){
     console.log("request index page")
     res.render("index", {title: "hi"})
     return
+})
+
+//get all blogs in database
+router.get("/home", async function(req,res,next){
+    try{
+        const [rows, fields] = await pool.query("SELECT * FROM Blogs;");
+        return res.json(rows);
+    } catch(err){
+        return next(err)
+    }
+})
+
+router.get("/blogs", async function(req,res,next){
+    try{
+        const [rows,fields] = await pool.query("SELECT * FROM Blogs;");
+        return res.json(rows)
+    }catch(err){
+        return next(err)
+    }
+})
+
+router.get("/blogs/:blogID", async function(req,res,next){
+    try{
+        const [rows, fields] = await pool.query("SELECT * FROM Blogs where Blog_ID = ?;",
+        [req.param.blogID])
+        return res.json(rows)
+    }catch(err){
+        return next(err)
+    }
 })
 
 exports.router = router;
