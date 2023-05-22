3c540cb5d1ee54eb771ee5b9cdf3a31d458dc4e9
chartnayodom <88505381+chartnayodom@users.noreply.github.com>
2023-05-22 13:38:10 +0700

    Update index.js
    fix role bug

--- Project_R (Publish)/middleware/index.js
+++ Project_R (Publish)/middleware/index.js
@@ -26,18 +26,21 @@ async function isLoggedIn (req, res, next) {
     if (!token) {
         return res.status(401).send('You are not logged in by not found token')
     }
-    if (token.role == 'user') {
+    // console.log(token.role)
+    if (token.role = 'user') {
         // Set user
+        // console.log('get role user')
         const [users] = await pool.query(
             'SELECT user_id, user_fname, user_lname, user_sign ' +
             'FROM user WHERE user_id = ?', [token.user_id]
         )
         req.user = users[0]
         req.user = Object.assign(req.user,{role : 'user'})
-        console.log(req.user)
+        // req.role = "user"
+        // console.log(req.user)
         next()
     }
-    else if (token.role == 'admin') {
+    else if (token.role = 'admin') {
         // Set admin
         const [users] = await pool.query(
             'SELECT Admin_ID, Admin_Alias' +
@@ -45,7 +48,7 @@ async function isLoggedIn (req, res, next) {
         )
         req.user = users[0]
         req.user = Object.assign(req.user,{role : 'admin'})
-        req.role = "admin"
+        // req.role = "admin"
         console.log(req.user)
         next()
     }
